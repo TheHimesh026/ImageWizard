6433e0855e957d486985536911430ad9f0eef727
Netaji <Netaji@gmail.com>
2024-10-09 16:40:39 +0530

    added cors to all as it was not working

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,9 @@ import routes from "./routes/index.js";
 const PORT = process.env.PORT || 2626;
 
 //middlewares
-app.use(
-  cors({
-    origin: "https://pixelshiftlab.web.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
